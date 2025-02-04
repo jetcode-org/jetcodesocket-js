7fb85608d17b10da0da1563273b5cc8ebc8d27da
nilov <nilov@glavweb.ru>
2025-02-04 18:12:53 +0700

    - The algorithm for connecting to the server has been changed - the step with join Lobby() has been added. - The names of actions have been changed. - The joinLobby() and leaveLobby() methods are implemented. - Name and category have been removed from parsing, they are no longer passed to RECEIVE_DATA. - Fixed bugs.

--- jetcodesocket.js
+++ jetcodesocket.js
@@ -1,21 +1,48 @@
 class JetcodeSocket {
-    static LOBBY_LEAVE = 'ACTION_LOBBY_LEAVE';
-    static RECEIVE_DATA = 'ACTION_RECEIVE_DATA';
-    static LOBBY_NEW_MEMBER = 'ACTION_LOBBY_NEW_MEMBER';
-    static GAME_STARTED = 'ACTION_GAME_STARTED';
-    static GAME_STOPPED = 'ACTION_GAME_STOPPED';
+    static JOIN_LOBBY = 'JOIN_LOBBY';
+    static LEAVE_LOBBY = 'LEAVE_LOBBY';
+    static SEND_DATA = 'SEND_DATA';
+
+    static JOINED = 'JOINED';
+    static RECEIVE_DATA = 'RECEIVE_DATA';
+    static MEMBER_JOINED = 'MEMBER_JOINED';
+    static MEMBER_LEFT = 'MEMBER_LEFT';
+    static GAME_STARTED = 'GAME_STARTED';
+    static GAME_STOPPED = 'GAME_STOPPED';
+    static ERROR = 'ERROR';
 
     constructor(socketUrl = 'ws://localhost:17500') {
         this.socketUrl = socketUrl;
         this.socket = null;
+
+        this.defaultParameters = {
+            'LobbyAutoCreate': true,
+            'MaxMembers': 2,
+            'MinMembers': 2,
+            'StartGameWithMembers' : 2
+        }
     }
 
-    connect(gameToken, lobbyId, parameters = {}) {
+    connect(gameToken, lobbyId = null, inParameters = {}) {
+        const parameters = { ...this.defaultParameters, ...inParameters};
+
         return new Promise((resolve, reject) => {
             this.socket = new WebSocket(this.socketUrl);
 
             this.socket.onopen = () => {
-                resolve(new JetcodeSocketConnection(this.socket, gameToken, lobbyId, parameters, 200, true));
+                const connection = new JetcodeSocketConnection(
+                    this.socket,
+                    gameToken,
+                    lobbyId,
+                    parameters
+                );
+
+                connection.joinLobby(gameToken, lobbyId, parameters)
+                    .then((assignedLobbyId) => {
+                        connection.lobbyId = assignedLobbyId;
+                        resolve(connection);
+                    })
+                    .catch(reject);
             };
 
             this.socket.onerror = (error) => {
@@ -26,93 +53,141 @@ class JetcodeSocket {
 }
 
 class JetcodeSocketConnection {
-    constructor(socket, gameToken, lobbyId, parameters, statusCode, successfull) {
+    constructor(socket, gameToken, lobbyId = 0) {
         this.socket = socket;
-        this.statusCode = statusCode;
-        this.successfull = successfull;
+        this.lobbyId = lobbyId;
+        this.memberId = null;
         this._connects = {};
+
+        this._connectActions = [
+            JetcodeSocket.RECEIVE_DATA,
+            JetcodeSocket.MEMBER_JOINED,
+            JetcodeSocket.MEMBER_LEFT,
+            JetcodeSocket.GAME_STARTED,
+            JetcodeSocket.GAME_STOPPED,
+            JetcodeSocket.ERROR
+        ];
 
         this._listenSocket();
     }
 
     _listenSocket() {
         this.socket.onmessage = (event) => {
-            const [action, parameters, name, category, value] = this._parse(event.data)
+            const [action, parameters, value] = this._parse(event.data)
 
-                // console.log(this._connects);
+            if (action === JetcodeSocket.RECEIVE_DATA) {
+                if (this._connects[JetcodeSocket.RECEIVE_DATA]) {
+                    this._connects[JetcodeSocket.RECEIVE_DATA](value, parameters, parameters?.MemberId === this.memberId);
+                }
 
-            switch (action) {
-                case JetcodeSocket.GAME_STARTED:
-                    this._connects[JetcodeSocket.GAME_STARTED](parameters);
-                    break
-                case JetcodeSocket.RECEIVE_DATA:
-                    if (this._connects[JetcodeSocket.RECEIVE_DATA]) {
-                        this._connects[JetcodeSocket.RECEIVE_DATA](name, value, category, parameters);
-                    }
-                    break
-                case JetcodeSocket.LOBBY_NEW_MEMBER:
-                    this._connects[JetcodeSocket.LOBBY_NEW_MEMBER](parameters);
-                    break
-                case JetcodeSocket.LOBBY_LEAVE:
-                    this._connects[JetcodeSocket.LOBBY_LEAVE](parameters);
-                    break
-                case JetcodeSocket.GAME_STOPPED:
-                    this._connects[JetcodeSocket.GAME_STOPPED](parameters);
-                    break
+            } else if (action === JetcodeSocket.MEMBER_JOINED) {
+                if (this._connects[JetcodeSocket.MEMBER_JOINED]) {
+                    this._connects[JetcodeSocket.MEMBER_JOINED](parameters, parameters?.MemberId === this.memberId);
+                }
+
+            } else if (action === JetcodeSocket.MEMBER_LEFT) {
+                if (this._connects[JetcodeSocket.MEMBER_LEFT]) {
+                    this._connects[JetcodeSocket.MEMBER_LEFT](parameters, parameters?.MemberId === this.memberId);
+                }
+
+            } else if (this._connects[action]) {
+                this._connects[action](parameters);
             }
-
-
-            // console.log("Received message: " + event.data);
-            // console.log(action, parameters, name, category, value)
-            // console.log(action)
         }
     }
 
     connect(action, callback) {
+        if (this._connectActions.indexOf(action) === false) {
+            throw new Error('This actions is not defined.');
+        }
+
         this._connects[action] = callback;
     }
 
-    sendData(name, value, category=null, parametrs={}) {
-        let request = 'ACTION_RECEIVE_DATA\n';
-        for (const [key, value] of Object.entries(parametrs)) {
+    sendData(value, parameters={}) {
+        if (!this.lobbyId) {
+            throw new Error('You are not in the lobby!');
+        }
+
+        let request = `${JetcodeSocket.SEND_DATA}\n`;
+
+        for (const [key, value] of Object.entries(parameters)) {
             request += key + '=' + value + '\n';
         }
-        request += '\n' + name + '\n' + category + '\n' + value;
+
+        request += '\n' + value;
+
         this.socket.send(request);
+    }
+
+    joinLobby(gameToken, lobbyId, parameters = {}) {
+        return new Promise((resolve, reject) => {
+            if (!lobbyId) {
+                lobbyId = 0;
+            }
+
+            let request = `${JetcodeSocket.JOIN_LOBBY}\n`;
+            request += `GameToken=${gameToken}\n`;
+            request += `LobbyId=${lobbyId}\n`;
+
+            for (const [key, value] of Object.entries(parameters)) {
+                request += `${key}=${value}\n`;
+            }
+
+            this.socket.send(request);
+
+            this.connect(JetcodeSocket.JOINED, (responseParams) => {
+                if (responseParams.LobbyId && responseParams.MemberId) {
+                    this.lobbyId = responseParams.LobbyId;
+                    this.memberId = responseParams.MemberId;
+                    resolve(this.lobbyId);
+
+                } else {
+                    reject(new Error("Couldn't join the lobby"));
+                }
+            });
+        });
+    }
+
+    leaveLobby() {
+        if (!this.lobbyId) {
+            return;
+        }
+
+        let request = `${JetcodeSocket.LEAVE_LOBBY}\nLobbyId=${this.lobbyId}\n`;
+        this.socket.send(request);
+
+        this.lobbyId = null;
     }
 
     _parse(data) {
         let parsable = data.split('\n');
         let action = parsable[0];
-        let category = '';
-        let name = '';
         let value = '';
         let parameters = [];
-        let nextIs = 'parametrs';
+
+        let nextIs = 'parameters';
         for (let i = 1; i < parsable.length; i++) {
             const line = parsable[i];
-            if (line === '' && nextIs === 'parametrs') {
-                nextIs = 'name';
-                continue;
-            }
-            if (nextIs === 'parametrs') {
-                const splitted = line.split('=');
-                const parametr = splitted[0];
-                let paramValue = null;
-                if (splitted.length > 1) {
-                    paramValue = splitted[1];
-                }
-                parameters[parametr] = paramValue;
-            } else if (nextIs === 'name') {
-                name = line;
-                nextIs = 'category';
-            } else if (nextIs === 'category') {
-                category = line;
+
+            if (line === '' && nextIs === 'parameters') {
                 nextIs = 'value';
+
+            } else if (nextIs === 'parameters') {
+                const splitted = line.split('=');
+
+                const parameter = splitted[0];
+                parameters[parameter] = splitted.length > 1 ? splitted[1] : null;
+
             } else if (nextIs === 'value') {
                 value = value + line + "\n";
             }
         }
-        return [action, parameters, name, category, value];
+
+        if (value) {
+            value = value.slice(0, -1);
+        }
+
+        return [action, parameters, value];
     }
 }
